621556ed872b54832c5efa89e9b578b431a9b5a3
Benjamin Guest <benguest@gmail.com>
2015-05-28 01:30:23 -0400

    Add debug stuff

--- Segment16Sign.ino
+++ Segment16Sign.ino
@@ -4,10 +4,16 @@
 Segment16 display;
 
 void setup(){
-   Serial.begin(9600);
+  Serial.begin(9600);
+  while (!Serial) {}  // wait for Leonardo
+  Serial.println("Type any character to start");
+  while (Serial.read() <= 0) {}
+  delay(200);  // Catch Due reset problem
+  // assume the user typed a valid character and no reset happened 
 }
 
 void loop(){
+  Serial.println("LOOP");
   display.show();
-  delay(2);
+  delay(1000);
 }
