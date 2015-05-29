4dd54885f2ddc2b4c1b0ea30fdc7f61514bcc93c
Benjamin Guest <benguest@gmail.com>
2015-05-29 01:48:23 -0400

    Add ability to input alt characters

--- Segment16Sign.ino
+++ Segment16Sign.ino
@@ -2,18 +2,29 @@
 #include <Segment16.h>
 
 Segment16 display;
+uint32_t incomingByte = 0, input = 0;   // for incoming serial data
 
 void setup(){
   Serial.begin(9600);
-  while (!Serial) {}  // wait for Leonardo
+  while (!Serial) {}  // wait for Leonard
   Serial.println("Type any character to start");
   while (Serial.read() <= 0) {}
   delay(200);  // Catch Due reset problem
+  display.init();
   // assume the user typed a valid character and no reset happened 
 }
 
 void loop(){
-  Serial.println("LOOP");
+  if(Serial.available() > 0){
+    input = 0;
+    while(Serial.available() > 0){
+      incomingByte = Serial.read();
+      input = (input << 8) | incomingByte;
+    }
+    display.pushChar(input);
+    Serial.println(input, HEX);
+    //incomingByte = Serial.read();
+  }
   display.show();
-  delay(1000);
+  delay(5);
 }
