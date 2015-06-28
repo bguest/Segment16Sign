8b4cadc32b3819c8e66dbfb7c776d5a2a4465253
Benjamin Guest <benguest@gmail.com>
2015-06-27 22:40:14 -0400

    Add Serial1

--- Segment16Sign.ino
+++ Segment16Sign.ino
@@ -6,23 +6,33 @@ uint32_t incomingByte = 0, input = 0;   // for incoming serial data
 
 void setup(){
   Serial.begin(9600);
-  while (!Serial) {}  // wait for Leonard
-  Serial.println("Type any character to start");
-  while (Serial.read() <= 0) {}
+  Serial1.begin(9600);
+  while (!Serial || !Serial1) {}  // wait for Leonard
+
+  String prompt = "Type any character to start";
+  Serial.println(prompt);
+  Serial1.println(prompt);
+
+  while (Serial.read() <= 0 || Serial1.read() <= 0) {}
   delay(200);  // Catch Due reset problem
   display.init();
-  // assume the user typed a valid character and no reset happened 
+  // assume the user typed a valid character and no reset happened
 }
 
 void loop(){
-  if(Serial.available() > 0){
+  if(Serial.available() > 0 || Serial1.available() > 0){
     input = 0;
     while(Serial.available() > 0){
       incomingByte = Serial.read();
       input = (input << 8) | incomingByte;
     }
+    while(Serial1.available() > 0){
+      incomingByte = Serial.read();
+      input = (input << 8) | incomingByte;
+    }
     display.pushChar(input);
     Serial.println(input, HEX);
+    Serial1.println(input, HEX);
     //incomingByte = Serial.read();
   }
   display.show();
